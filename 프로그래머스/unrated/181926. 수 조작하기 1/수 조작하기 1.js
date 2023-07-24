4829858b49615695c5a7ad816fca9229d8542569
jin62413 <71061884+jin62413@users.noreply.github.com>
2023-07-24 23:07:23 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.98 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,16 @@
+function solution(n, control) {
+    let move = control.split('');
+    move.map((item) => {
+        if(item === 'w') {
+            n += 1;
+        } else if(item === 's') {
+            n -= 1;
+        } else if(item === 'd') {
+            n += 10;
+        } else if(item === 'a') {
+            n -= 10;
+        }
+        return n;
+    })
+    return n;
+}
