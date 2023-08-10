7542061e31ff3d558624266bf9c1f6703616c7cf
jin62413 <71061884+jin62413@users.noreply.github.com>
2023-08-10 21:22:42 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,8 +1,7 @@
 function solution(myString, pat) {
-    
     str = [...myString];
     let answer = str.map((item) => item === 'A' ? item = 'B' : item = 'A').join('');
-    console.log(answer);
+
     if(answer.includes(pat) === true) {
         return 1;
     } else {
