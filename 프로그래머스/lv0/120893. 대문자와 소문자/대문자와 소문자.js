19da2e6071c7e2d28a63f526c7938806eb85a516
jin62413 <71061884+jin62413@users.noreply.github.com>
2023-08-17 02:14:53 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.49 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,5 +1,5 @@
 function solution(my_string) {
     let arr = [];
-    [...my_string].map(item => (item.match(/[A-Z]/) !== null) ? arr.push(item.toLowerCase()) : arr.push(item.toUpperCase()));
+    [...my_string].map(item => (item.match(/[A-Z]+/g) !== null) ? arr.push(item.toLowerCase()) : arr.push(item.toUpperCase()));
     return arr.join('');
 }
